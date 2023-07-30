0cc2bc7ade106382a32e7c9e41f66a2b3c6b5137
weiowyw <haxjlebatop@gmail.com>
2023-07-30 21:59:26 +0500

    firsada

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+
+// 1.
+// const number = +prompt('Введите число между 0 и 3', '');
+//
+// switch (number){
+//     case 0:
+//         alert('Вы ввели число 0');
+//         break;
+//     case 1:
+//         alert('Вы ввели число 1');
+//         break;
+//     case 2:
+//     case 3:
+//         alert('Вы ввели число 2, а может и 3');
+//         break;
+//     default:
+//         alert('Вы ввели неверное значние');
+//         break;
+// }
+
+
+// 2.
+
+// for(let i = 2; i < 11; i++){
+//     if(i % 2 === 0){
+//         console.log(`${i}, `);
+//     }
+// }
